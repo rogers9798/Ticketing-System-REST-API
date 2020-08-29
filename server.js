f09569ca8383bd0ed109bc5a5f905ff0b2fe2731
Sachin Kumar <sac9798@gmail.com>
2020-08-29 10:35:35 +0530

    node server inititated

---
+++ server.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/ticket", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {});
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname + "/views"));
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error("File Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+var port = 3000;
+app.listen(port, function () {
+  console.log("Express app listening on port ",port);
+});
