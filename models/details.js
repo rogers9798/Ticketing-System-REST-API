408828a9faa894c4b9fc0e65494a069e6e49b92e
Sachin Kumar <sac9798@gmail.com>
2020-08-31 07:10:45 +0530

    ticket schema updated

--- models/details.js
+++ models/details.js
@@ -2,14 +2,38 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 (userSchema = new Schema({
-  ticket_id: String,
-  username: String,
-  phone: Number,
-  timings: {
-    type: Date,
-    default: Date.now()
+  ticket_id: {
+    type: String,
   },
-  ticket_expired: Boolean,
+  username: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  ticket_date: {
+    type: String,
+    required: true,
+  },
+  slot: {
+    hrs: {
+      type: Number,
+      required: true,
+    },
+    mins: {
+      type: Number,
+      required: true,
+    },
+  },
+  booking_date: {
+    type: String,
+    required: true,
+  },
+  ticket_expired: {
+    type: Boolean,
+  },
 })),
   (Details = mongoose.model("Details", userSchema));
 
